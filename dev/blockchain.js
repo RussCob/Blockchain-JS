8f1bc7d9674de89822c5da4cf196c824210543c9
Blackfb <62447669+Blackfb@users.noreply.github.com>
2021-02-09 21:00:42 -0500

    update

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,4 +1,46 @@
+
 function Blockchain() {
     this.chain = [];
-    this.newTransactions = [];
+    this.pendingTransactions = [];
 }
+
+Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
+    const newBlock = {
+        index      : this.chain.length + 1,
+        timestap   : Date.now(),
+        transction : this.pendingTransactions,
+        nonce      : nonce,
+        hash       : hash,
+        previousBlockHash : previousBlockHash
+    };
+
+    this.pendingTransactions = [];
+    this.chain.push(newBlock);
+
+    return newBlock;
+
+}
+
+Blockchain.prototype.getLastBlock = function() {
+    return this.chain[this.chain.length - 1];
+}
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient){
+    const newTransaction = {
+        amount    : amount,
+        sender    : sender,
+        recipient : recipient
+    };
+
+    this.pendingTransactions.push(newTransaction);
+
+    return this.getLastBlock()['index'] + 1;
+}
+
+Blockchain.prototype.hashBlock = function(blockData) {
+
+    // ... return '9034563434sddfuabu3b4ub' (SHA256 Hash Generator)
+}
+
+
+module.exports = Blockchain;
